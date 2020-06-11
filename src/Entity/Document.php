1d85994551210d191c5c181e9f236301286ea11f
Julien Gault <jugault45@gmail.com>
2020-06-11 17:51:46 +0200

    correction travis

--- src/Entity/Document.php
+++ src/Entity/Document.php
@@ -6,6 +6,8 @@ use App\Repository\DocumentRepository;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
@@ -23,6 +25,7 @@ class Document
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez entrer un nom de fichier")
      */
     private $name;
 
@@ -33,6 +36,11 @@ class Document
     /**
      * @Vich\UploadableField(mapping="document_file", fileNameProperty="document")
      * @var File
+     * @Assert\File(
+     *     mimeTypes={"application/pdf","application/msword","application/vnd.oasis.opendocument.text","text/plain"},
+     *     mimeTypesMessage="Veuillez insérer un fichier au format {{ types }}"
+     * )
+     * @Assert\NotBlank(message="Veuillez insérer un fichier")
      */
     private $documentFile;
     /**
@@ -63,22 +71,32 @@ class Document
         return $this->document;
     }
 
-    public function setDocument(string $document): self
+    public function setDocument(?string $document): self
     {
         $this->document = $document;
 
         return $this;
     }
-    public function setDocumentFile(File $image = null): Document
+
+    public function setDocumentFile(File $image): Document
     {
         $this->documentFile = $image;
-        if ($image) {
-            $this->updatedAt = new DateTime('now');
-        }
+        $this->updatedAt = new DateTime('now');
+        return $this;
     }
 
     public function getDocumentFile(): ?File
     {
         return $this->documentFile;
+    }
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+    public function setUpdatedAt(DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 }
