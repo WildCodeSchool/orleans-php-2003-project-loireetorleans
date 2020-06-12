cd16d44b54464e1f959d0a462d175f3db6031af0
Julien Gault <jugault45@gmail.com>
2020-06-12 09:00:00 +0200

    correction de casse modif du user pour la demo

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -22,7 +22,7 @@ class UserFixtures extends Fixture
         $faker = Faker\Factory::create('fr_FR');
         $subscriber = new User();
         $subscriber->setLogin('hadef');
-        $subscriber->setRoles(['ROLE_SUBSCRIBER']);
+        $subscriber->setRoles(['ROLE_AMBASSADEUR']);
         $subscriber->setFirstname($faker->firstName);
         $subscriber->setLastname($faker->lastName);
         $subscriber->setEmail($faker->email);
