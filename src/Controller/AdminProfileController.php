1121b88d82047610aad83e4d5e6b2fd15ab78353
Julien Gault <jugault45@gmail.com>
2020-06-16 19:02:18 +0200

    création de la requete préparé

--- src/Controller/AdminProfileController.php
+++ src/Controller/AdminProfileController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -12,11 +14,14 @@ class AdminProfileController extends AbstractController
 {
     /**
      * @Route("/", name="_index")
+     * @param UserRepository $userRepository
+     * @return Response
      */
-    public function index()
+    public function index(UserRepository $userRepository) : Response
     {
+        $users = $userRepository->findAllWhithoutAdmin();
         return $this->render('admin_profile/index.html.twig', [
-
+            'users' => $users,
         ]);
     }
 }
