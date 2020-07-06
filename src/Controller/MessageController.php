1087f94dff6e1ded8a821c019137c98843fafca1
Julien Gault <jugault45@gmail.com>
2020-07-06 19:11:51 +0200

    push*

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -84,10 +84,10 @@ class MessageController extends AbstractController
                 );
             }
         }
-        if ($message !== null) {
-            $idMessage = $message->getConversation()->getId();
-            $idDocument = $message->getConversation()->getDocument()->getId();
-        }
+
+        $idMessage = $message->getConversation()->getId();
+        $idDocument = $message->getConversation()->getDocument()->getId();
+
 
 
         if (in_array('ROLE_ADMINISTRATEUR', $user->getRoles())) {
