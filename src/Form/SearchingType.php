0d0bcb13088a694a2211e5ce71e4f53edf4e6a43
Damien <karli.damien@gmail.com>
2020-07-17 16:29:28 +0200

    suppression du token csrf et de [Search] dans l'URL

--- src/Form/SearchingType.php
+++ src/Form/SearchingType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchingType extends AbstractType
 {
@@ -12,6 +13,21 @@ class SearchingType extends AbstractType
         $builder
             ->add('search', SearchType::class, [
                 'required' => false,
-            ]);
+            ])
+            ->setMethod('GET')
+            ->getForm();
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'method' => 'get',
+            'csrf_protection' => false
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
     }
 }
